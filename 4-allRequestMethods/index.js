b2951b612fdc9720a42656edaeec8e0806ebc06b
josprimen <josprimen@alum.us.es>
2020-03-02 18:39:56 +0000

    minor fixes

--- 4-allRequestMethods/index.js
+++ 4-allRequestMethods/index.js
@@ -1,11 +1,7 @@
-//Modules
 var express = require('express');
 var bodyparser = require('body-parser');
-var nedb = require('nedb');
-//Variables
 var port = process.env.PORT || 3000;
 var BASE_URL = '/josprimenapi/v1';
-var DataStore = __dirname+"/store.db";
 
 var app = express();
 
@@ -27,32 +23,11 @@ console.log('Server Ready -> False');
 console.log(__dirname);
 /*##############################################################################################################*/
 
-//Data code
-var InitialContacts = [
+var contacts = [
     { name: 'josenri', tlf: 675132234 },
     { name: 'jesus', tlf: 897789898 },
     { name: 'natalia', tlf: 89987665 }
 ];
-
-var db = new nedb({
-	filename: DataStorea,
-	autoload: true //Load data automatically when starting the web server
-});
-
-//The first square brackets are in case some condition is placed when searching data in the database eg name: "pepe", tlf: 648. If you don't return them all.
-db.find({},(err, contacts)=>{
-	if(err) {
-		console.error("Error accesing DataBase");//console.error is like console.log for errors
-		process.exit(1);//We should turn off the program in case this fail. To do this we put a non zero number.
-	}
-	if(contacts.length == 0){
-		console.log("Empty DataBase");
-		db.insert(InitialContacts);
-	}else{
-		console.log(Date() + " - Data loaded");
-	}
-	
-});
 
 //Resources
 
