64d39c7a31f0f5621e87f29560f5757d52111f22
josprimen <josprimen@alum.us.es>
2020-04-13 18:05:35 +0000

    data fixed and search added

--- 7-oliveBackend/oliveAPI/index.js
+++ 7-oliveBackend/oliveAPI/index.js
@@ -36,8 +36,8 @@ oliveAPI.methods = function(app, BASE_URL, db, InitialDeliveryNotes, alldata) {
             );
         });
     });
-	
-	app.get(BASE_URL + '/olive/loadAllData', (req, res) => {
+
+    app.get(BASE_URL + '/olive/loadAllData', (req, res) => {
         console.log(Date() + ' - GET /olive/loadAllData');
         db.insertMany(alldata);
         db.find({}).toArray((err, olive) => {
@@ -63,24 +63,207 @@ oliveAPI.methods = function(app, BASE_URL, db, InitialDeliveryNotes, alldata) {
     });
 
     app.get(BASE_URL + '/olive', (req, res) => {
-        console.log(Date() + ' - GET /olive');
-		var limit = parseInt(req.query.limit);
-		var offset = parseInt(req.query.offset);
-        db.find({}).skip(offset).limit(limit).toArray((err, olive) => {
-            if (err) {
-                console.error('Error accesing DataBase');
-                res.sendStatus(500);
-            }
-            res.send(
-                olive.map(x => {
-                    delete x._id;
-                    return x;
-                })
-            );
-        });
+        var limit = parseInt(req.query.limit);
+        var offset = parseInt(req.query.offset);
+        var afrom = Number(req.query.from);
+        var ato = Number(req.query.to);
+        var rdto = parseInt(req.query.RDTO);
+        var humidity = Number(req.query.HUMEDAD);
+        var acidity = Number(req.query.ACIDEZ);
+        var kgs = Number(req.query.KGSACEITUNA);
+        var day = Number(req.query.DIA);
+        var month = Number(req.query.MES);
+        var year = Number(req.query.ANYO);
+		search = false;
+		console.log(search);
+		console.log(rdto);
+        if (afrom && ato && rdto) { //URL form www.server/resource?searchparam&from=value&to=value with this format search param will be set to 0 and will get in the if
+			search = true;
+			console.log(Date() + ' - GET /olive search by rdto');
+            db
+                .find({ RDTO: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+
+        if (afrom && ato && humidity) {
+			search = true;
+			console.log(Date() + ' - GET /olive search by humidity');
+            db
+                .find({ HUMEDAD: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+
+        if (afrom && ato && acidity) {
+			search = true;
+			console.log(Date() + ' - GET /olive search by acidity');
+            db
+                .find({ ACIDEZ: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+
+        if (kgs) {
+			search = true;
+			console.log(Date() + ' - GET /olive search by kilograms');
+            db
+                .find({ KGSACEITUNA: kgs })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+		
+		if (afrom && ato && year) {
+			search = true;
+			console.log(Date() + ' - GET /olive search by year interval');
+            db
+                .find({ ANYO: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+		
+		if (afrom && ato && year && month) {
+			console.log(Date() + ' - GET /olive search by month interval in a year');
+			search = true
+            db
+                .find({ ANYO: year, MES: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+		
+		if (afrom && ato && year && month && day) {
+			console.log(Date() + ' - GET /olive search by day interval in a month of the year');
+			search = true;
+            db
+                .find({ ANYO: year, MES: month, DIA: { $gte: afrom, $lte: ato } })
+                .skip(offset)
+                .limit(limit)
+                .toArray((err, olive) => {
+                    if (err) {
+                        console.error('Error accesing DataBase');
+                        res.sendStatus(500);
+                    }
+                    res.send(
+                        olive.map(x => {
+                            delete x._id;
+                            return x;
+                        })
+                    );
+                });
+        }
+		
+		if(!search){
+			console.log(Date() + ' - GET /olive');
+			db
+            .find({})
+            .skip(offset)
+            .limit(limit)
+            .toArray((err, olive) => {
+                if (err) {
+                    console.error('Error accesing DataBase');
+                    res.sendStatus(500);
+                }
+                res.send(
+                    olive.map(x => {
+                        delete x._id;
+                        return x;
+                    })
+                );
+            });
+		   
+		   }
+/*
+        db
+            .find({})
+            .skip(offset)
+            .limit(limit)
+            .toArray((err, olive) => {
+                if (err) {
+                    console.error('Error accesing DataBase');
+                    res.sendStatus(500);
+                }
+                res.send(
+                    olive.map(x => {
+                        delete x._id;
+                        return x;
+                    })
+                );
+            });*/
     });
 
-app.get(BASE_URL + '/olive/:year', (req, res) => {
+    app.get(BASE_URL + '/olive/:year', (req, res) => {
         var year = req.params.year;
         console.log(Date() + ' - GET /olive/' + year);
         db.find({ ANYO: year }).toArray((err, olive) => {
@@ -95,9 +278,9 @@ app.get(BASE_URL + '/olive/:year', (req, res) => {
                 })
             );
         });
-    });	
+    });
 
-app.get(BASE_URL + '/olive/:year/:month', (req, res) => {
+    app.get(BASE_URL + '/olive/:year/:month', (req, res) => {
         var month = req.params.month;
         var year = req.params.year;
         var date = '/' + year + '/' + month;
@@ -114,9 +297,9 @@ app.get(BASE_URL + '/olive/:year/:month', (req, res) => {
                 })
             );
         });
-    });	
+    });
 
-app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
+    app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         var day = req.params.day;
         var month = req.params.month;
         var year = req.params.year;
@@ -134,15 +317,15 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
                 })
             );
         });
-    });	
+    });
 
     app.get(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
         var ticket = req.params.ticket;
-		var day = req.params.day;
+        var day = req.params.day;
         var month = req.params.month;
         var year = req.params.year;
         var date = '' + day + '/' + month + '/' + year;
-        console.log(Date() + ' - GET /olive/' + date + '/' +ticket);
+        console.log(Date() + ' - GET /olive/' + date + '/' + ticket);
         db.find({ DIA: day, MES: month, ANYO: year, TICKET: ticket }).toArray((err, olive) => {
             if (err) {
                 console.error('Error accesing DataBase');
@@ -157,7 +340,6 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         });
     });
 
-  
     /*#PP------------------------------ALLOWED POST AND PUT---------------------------*/
 
     app.post(BASE_URL + '/olive', (req, res) => {
@@ -166,8 +348,8 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         db.insertOne(deliveryNote);
         res.sendStatus(201);
     });
-	
-	app.put(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
+
+    app.put(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
         var deliverybody = req.body;
         var ticket = req.params.ticket;
         console.log(Date() + ' - PUT /contacts/' + ticket);
@@ -175,13 +357,12 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
             res.sendStatus(409);
             console.warn(Date() + ' - Hacking attempt!');
         } else {
-            db.updateOne({ TICKET: ticket}, {$set:deliverybody}, (err) => {
+            db.updateOne({ TICKET: ticket }, { $set: deliverybody }, err => {
                 console.log(Date() + ' - One delivery note updated.');
             });
             res.sendStatus(200);
         }
     });
-	
 
     /*#DE------------------------------DELETES---------------------------*/
 
@@ -192,46 +373,46 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         }); //square brackets are in case some condition is placed when removing data in the database
         res.sendStatus(200);
     });
-	
-	app.delete(BASE_URL + '/olive/:year', (req, res) => {
-		var year = req.params.year;
+
+    app.delete(BASE_URL + '/olive/:year', (req, res) => {
+        var year = req.params.year;
         console.log(Date() + ' - DELETE /olive/' + year);
-        db.deleteMany({ANYO:year}, {}, (err, numDelete) => {
+        db.deleteMany({ ANYO: year }, {}, (err, numDelete) => {
             console.log(Date() + ' - Several olive delivery notes deleted.');
         }); //square brackets are in case some condition is placed when removing data in the database
         res.sendStatus(200);
     });
-	
-	app.delete(BASE_URL + '/olive/:year/:month', (req, res) => {
-		var year = req.params.year;
-		var month = req.params.month;
-		var date = '/' + year + '/' + month;
+
+    app.delete(BASE_URL + '/olive/:year/:month', (req, res) => {
+        var year = req.params.year;
+        var month = req.params.month;
+        var date = '/' + year + '/' + month;
         console.log(Date() + ' - DELETE /olive/' + date);
-        db.deleteMany({ANYO:year, MES: month}, {}, (err, numDelete) => {
+        db.deleteMany({ ANYO: year, MES: month }, {}, (err, numDelete) => {
             console.log(Date() + ' - Several olive delivery notes deleted.');
         }); //square brackets are in case some condition is placed when removing data in the database
         res.sendStatus(200);
     });
-	
-	app.delete(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
-		var year = req.params.year;
-		var month = req.params.month;
-		var day = req.params.day;
-		var date = '/' + year + '/' + month + '/' + day;
+
+    app.delete(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
+        var year = req.params.year;
+        var month = req.params.month;
+        var day = req.params.day;
+        var date = '/' + year + '/' + month + '/' + day;
         console.log(Date() + ' - DELETE /olive/' + date);
-        db.deleteMany({ANYO:year, MES: month, DIA: day}, {}, (err, numDelete) => {
+        db.deleteMany({ ANYO: year, MES: month, DIA: day }, {}, (err, numDelete) => {
             console.log(Date() + ' - Several olive delivery notes deleted.');
         }); //square brackets are in case some condition is placed when removing data in the database
         res.sendStatus(200);
     });
-	
-	app.delete(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
-		var year = req.params.year;
-		var month = req.params.month;
-		var day = req.params.day;
-		var ticket = req.params.ticket;
+
+    app.delete(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
+        var year = req.params.year;
+        var month = req.params.month;
+        var day = req.params.day;
+        var ticket = req.params.ticket;
         console.log(Date() + ' - DELETE /olive/' + ticket);
-        db.deleteOne({ANYO:year, MES: month, DIA: day, TICKET:ticket}, {}, (err, numDelete) => {
+        db.deleteOne({ ANYO: year, MES: month, DIA: day, TICKET: ticket }, {}, (err, numDelete) => {
             console.log(Date() + ' - Olive delivery note deleted.');
         }); //square brackets are in case some condition is placed when removing data in the database
         res.sendStatus(200);
@@ -247,14 +428,14 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
     });
 
     /*#PO------------------------------NOT ALLOWED POSTS---------------------------*/
-	
-	app.post(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
+
+    app.post(BASE_URL + '/olive/:year/:month/:day/:ticket', (req, res) => {
         var ticket = req.params.ticket;
         console.log(Date() + ' - POST /contacts/' + ticket);
         res.sendStatus(405);
     });
-	
-	app.post(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
+
+    app.post(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         var day = req.params.day;
         var month = req.params.month;
         var year = req.params.year;
@@ -262,26 +443,24 @@ app.get(BASE_URL + '/olive/:year/:month/:day', (req, res) => {
         console.log(Date() + ' - POST /contacts/' + date);
         res.sendStatus(405);
     });
-	
-	app.post(BASE_URL + '/olive/:year/:month', (req, res) => {
+
+    app.post(BASE_URL + '/olive/:year/:month', (req, res) => {
         var month = req.params.month;
         var year = req.params.year;
         var date = '' + month + '/' + year;
         console.log(Date() + ' - POST /contacts/' + date);
         res.sendStatus(405);
     });
-	
-	app.post(BASE_URL + '/olive/:year', (req, res) => {
+
+    app.post(BASE_URL + '/olive/:year', (req, res) => {
         var year = req.params.year;
         console.log(Date() + ' - POST /contacts/' + year);
         res.sendStatus(405);
     });
-	
-	app.post(BASE_URL + '/olive/:ticket', (req, res) => {
+
+    app.post(BASE_URL + '/olive/:ticket', (req, res) => {
         var ticket = req.params.ticket;
         console.log(Date() + ' - POST /contacts/' + ticket);
         res.sendStatus(405);
     });
-	
-	
 };
