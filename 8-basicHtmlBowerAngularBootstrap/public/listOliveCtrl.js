8a73736dc8cc6c5077c677df8586611210c239c0
josprimen <josprimen@alum.us.es>
2020-05-06 12:36:30 +0000

    insert form created and button to delete one obejct added

--- 8-basicHtmlBowerAngularBootstrap/public/listOliveCtrl.js
+++ 8-basicHtmlBowerAngularBootstrap/public/listOliveCtrl.js
@@ -5,11 +5,31 @@ angular
         var url = '/josprimenapi/v1/olive';
 
         function getData() {
-            $http.get(url).then(function(response) {
+            $http.get(url).then(function(res) {
                 console.log('Getting ' + url);
-                $scope.olivedata = response.data;
+                $scope.olivedata = res.data;
+				//$scope.success_status = res.status;
             });
         }
         getData();
+		
+		$scope.newolivedata = function(){
+			$http.post(url, $scope.newolive).then(function(req, res){
+				console.log("Posting to " + url);
+				getData();
+				$scope.success_status = res.status;
+			});
+		};
+		
+		$scope.deleteticket = function(day, month, year, ticket){
+			
+			$http.delete(url + "/" + year + "/" + month + "/" + day + "/" + ticket).then(function(req,res){
+				console.log("Deleting delivery note with number: " + ticket);
+				//$scope.success_status = res.status + " delivery note number " + ticket + " deleted";
+				getData();
+			});
+			
+		};
+		
     }
 ]);
